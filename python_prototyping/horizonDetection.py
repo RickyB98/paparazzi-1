a4438c3b6e4d5a1ee17d49ae2f11945b3aa07783
SUPfeiffer <sven.pfeiffer@hotmail.com>
2020-03-06 11:02:30 +0100

    added RANSAC identification of the principal horizon

--- python_prototyping/horizonDetection.py
+++ python_prototyping/horizonDetection.py
@@ -9,13 +9,15 @@ import sys
 from cv2 import cv2
 import numpy as np 
 import matplotlib.pyplot as plt 
-import time
+import random
 
 # open image
 #img_path = 'datasets/cyberzoo_poles/20190121-135009/'
 #img_nmb = 80211420
 img_path = 'datasets/cyberzoo_poles_panels/20190121-140205/'
-img_nmb = 93349216 #96849201
+img_nmb = 96849201 #93349216 
+#img_path = 'datasets/cyberzoo_canvas_approach/20190121-151448/'
+#img_nmb = 54248124
 
 img_name = img_path + str(img_nmb) + ".jpg"
 
@@ -133,12 +135,9 @@ def snakeHorizon(img):
     # edge detection
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     cl = clahe.apply(gray)
-    edges = cv2.Canny(cl,20,80)
+    edges = cv2.Canny(cl,15,80)
     cv2.imshow('edges',edges)
     
-
-
-    obstacles = np.zeros(img.shape[0])
     x = 0
     y = 0
     y_max = 0
@@ -152,6 +151,7 @@ def snakeHorizon(img):
             horizon[y] = x
             y_min = followHorizonLeft(edges, [x,y], y_max)
             y_max = followHorizonRight(edges, [x,y])
+            # could go right first and use distance to decide if we want to overwrite when moving left
             y = y_max + 1
             x = 0
             # if the segment is too short, scrap it
@@ -161,8 +161,49 @@ def snakeHorizon(img):
         #break
     #print("exit while")
 
+def ransacHorizonLine(img):
+    global horizon
+    threshold = 5
+    elements = horizon.shape[0]             # pylint: disable=E1136     # pylint/issues/3139
+    best_error = threshold * (elements+1)
+    error = np.zeros(10)
+    m = np.zeros(10)
+    b = np.zeros(10)
+    for i in range(10):
+        [s1,s2] = random.sample(range(elements),2)
+        m[i] = (horizon[s2]-horizon[s1])/(s2-s1)
+        b[i] = horizon[s1] -  m[i]*s1
 
 
+        for j in range(elements):
+            # draw on image
+            x = int(np.round(m[i]*j + b[i]))
+            if (x>=0 and x<img.shape[1]):
+                img[j][x][0] = 255
+                img[j][x][1] = 155
+                img[j][x][2] = 0
+
+            delta = np.abs(horizon[j] - m[i]*j - b[i])
+            if delta < threshold:
+                error[i] = error[i] + delta
+            else:
+                error[i] = error[i] + threshold
+        
+        if error[i] < best_error:
+            best_error = error[i]
+            best_m = m[i]
+            best_b = b[i]
+    
+    best_horizon = np.zeros(elements)
+    for j in range(elements):
+        x = int(np.round(best_m*j + best_b))
+        img[j][x][0] = 0
+        img[j][x][1] = 255
+        img[j][x][2] = 0
+        best_horizon[j] = x
+
+    #cv2.imshow('horizon_lines',img)
+    return best_horizon
 
 
 
@@ -184,6 +225,8 @@ eq = cv2.equalizeHist(gray)
 #floor = floorDetect(img)
 #cv2.imshow('floor',floor)
 snakeHorizon(img)
+img_ransac = cv2.imread(img_name)
+best_horizon = ransacHorizonLine(img_ransac)
 img_w_track = cv2.imread(img_name)
 img_w_horizon = cv2.imread(img_name)
 
@@ -198,6 +241,9 @@ for i in range(img.shape[0]):
     img_w_horizon[i][value][0] = 0
     img_w_horizon[i][value][1] = 0
     img_w_horizon[i][value][2] = 255
+    img_w_horizon[i][int(best_horizon[i])][0] = 0
+    img_w_horizon[i][int(best_horizon[i])][1] = 255
+    img_w_horizon[i][int(best_horizon[i])][2] = 0
     img_w_track[i][value][0] = 0
     img_w_track[i][value][1] = 0
     img_w_track[i][value][2] = 255
