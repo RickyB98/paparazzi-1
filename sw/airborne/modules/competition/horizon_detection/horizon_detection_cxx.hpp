ddd272dd758d5841caef7bbfaeb4fc7e6b8be425
Riccardo Bello <riccardobello98@gmail.com>
2020-03-26 19:08:03 +0100

    Move declarations around

--- sw/airborne/modules/competition/horizon_detection/horizon_detection_cxx.hpp
+++ sw/airborne/modules/competition/horizon_detection/horizon_detection_cxx.hpp
@@ -3,8 +3,10 @@
 
 #include "horizon_detection.h"
 
-#include <opencv2/core/core.hpp>
 #include "modules/computer_vision/lib/vision/image.h"
+#include <opencv2/core/core.hpp>
+
+#define IMAGE_WIDTH 520
 
 cv::Mat image_edges(struct image_t *img);
 struct image_t *horizonDetection(struct image_t *img);
@@ -13,4 +15,4 @@ int followHorizonLeft(cv::Mat *edge_image, Dot *p, int y_lim, int *horizon);
 bool isFloor(struct image_t *img, int x, int y);
 void findHorizonCandidate(struct image_t *img, cv::Mat *edge_image, Dot *p);
 
-#endif //PAPARAZZI_HORIZON_DETECTION_HPP
+#endif // PAPARAZZI_HORIZON_DETECTION_HPP
